9aede9d1bff0d8b404298d82f05e8b50e7fea4e5
Uzun Ion <uzun.ion.pavel@gmail.com>
2022-01-19 18:13:17 +0200

    Register in AppServiceProvider RequestActivityLoggerInterface binding.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\DummyRequestActivityLogger;
+use App\Services\RequestActivityLoggerInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(RequestActivityLoggerInterface::class, function (){
+            return $this->app->make(DummyRequestActivityLogger::class);
+        });
     }
 
     /**
