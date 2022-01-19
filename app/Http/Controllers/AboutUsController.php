edf6f5b4e12a395feca2e0fa828034bc95c8b430
Uzun Ion <uzun.ion.pavel@gmail.com>
2022-01-19 22:29:52 +0200

    Correct naming of AboutUs page in view, routes and controller.

--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AboutUsController extends Controller
 {
     public function index() {
-        return view('about-us');
+        return view('aboutUs');
     }
 }
